6ac3bf637f7b4540f1a8416223c124d5ef35e26f
SIR-WIZIX <egorbezbabchenko@gmail.com>
2026-04-14 14:15:53 +0300

    Task1 init

--- Lab9/Blue/Blue.cs
+++ Lab9/Blue/Blue.cs
@@ -1,3 +1,22 @@
 ﻿namespace Lab9.Blue
 {
+  abstract class Blue
+  {
+    private string _input;
+
+    public string Input => _input;
+
+    protected Blue(string input)
+    {
+      _input = input;
+    }
+
+    abstract void Review();
+
+    virtual void ChangeText(string text)
+    {
+      _input = text;
+      Review();
+    }
+  }
 }
---
+++ Lab9/Blue/Task1.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab9.Blue;
+
+public class Task1 : Blue
+{
+  private string[] _output;
+
+  public string[] Output => _output.ToArray();
+
+  public Task1(string input) : base(input){}
+
+  public override void Review()
+  {
+
+  }
+
+  public override string ToString()
+  {
+    string ans = "";
+    foreach (string w in _output)
+    {
+      ans += w + ' ';
+    }
+
+    return ans.TrimEnd();
+  }
+
+}
